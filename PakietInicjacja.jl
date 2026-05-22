4d91473801d9f858c95b4c2ef853c3cc375296ec
Kamil <277464@student.pwr.edu.pl>
2026-05-22 14:03:00 +0200

    Nie zapisany plik lol

--- PakietInicjacja.jl
+++ PakietInicjacja.jl
@@ -6,7 +6,6 @@ authors="Michał Więcek, Igor Ciesielski, Kamil Burnecki, PWr",
 julia=v"1.11.7",
 plugins=[
 License(; name="GPL-3.0-or-later"),
-Git(; manifest=true),
 GitHubActions(; x86=true),
 Codecov(),
 Documenter{GitHubActions}(),
