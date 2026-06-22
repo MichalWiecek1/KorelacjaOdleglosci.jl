ff33d963c171c8d4d6619a24a5a497f3f81fdd25
Igor Ciesielski 277488 <igorcslsk@gmail.com>
2026-06-22 21:22:08 +0200

    uporządkowanie struktury repozytorium

--- src/HelperFunctions.jl
+++ src/HelperFunctions.jl
@@ -1,3 +1,51 @@
+function bin_U(x)
+    Bins = Vector{Int8}(undef, length(x)) 
+    @inbounds @simd for i in eachindex(x) 
+        Bins[i] = trunc(Int8, 5*x[i]) + 1 
+    end 
+    return Bins
+end
+
+function bin_N(x)
+    Bins = Vector{Int8}(undef, length(x)) 
+
+    @inbounds for i in eachindex(x)
+        xi = x[i]
+
+        if xi < 0
+            if xi < -1
+                if xi < -2
+                    Bins[i] = 1
+                else
+                    Bins[i] = 2
+                end
+            else
+                Bins[i] = 3
+            end
+        else
+            if xi < 1
+                Bins[i] = 4
+            elseif xi < 2
+                Bins[i] = 5
+            else
+                Bins[i] = 6
+            end
+        end
+    end
+
+    return Bins
+end
+
+function p_value(x,y,method,iter=1000)
+    stat = method(x,y)
+    hits = 0
+    kopia_y = copy(y)
+    for _ in 1:iter
+        hits += (method(x,shuffle!(kopia_y)) > stat)
+    end
+    return hits / iter
+end
+
 function dCov_M_slow(x,y)
     n = length(x)
     A = zeros(Float64,n,n)
